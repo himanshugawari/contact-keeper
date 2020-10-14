842160cb55aa18265f777fbd627c4524f00da1b1
unknown <himanshu25031991@gmail.com>
2020-10-14 12:16:54 +0530

    Routes to navbar, home & about

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About Contact Keeper</h1>
+      <p className='my-1'>
+        Full stack MERN contact manager with React hooks, context & JWT
+        authentication.
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong>1.0.0
+      </p>
+    </div>
+  );
+};
